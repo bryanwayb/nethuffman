c32c6d3edab628ead66fd76be9cb16c72270c6df
Bryan Way <bryanwayb@gmail.com>
2016-09-21 05:22:54 -0500

    Building unique binary tree

--- NetHuffman/Tree.cs
+++ NetHuffman/Tree.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace NetHuffman
 {
     public class Tree
     {
+        public Tree()
+        {
+        }
+
         public Tree(Dictionary<byte, Dictionary<uint, byte>> lookup)
         {
             Init(lookup);
@@ -65,7 +70,7 @@ namespace NetHuffman
             }
 
             Coder test = new Coder(this);
-            List<byte> clist = new List<byte>();
+            HashSet<byte> clist = new HashSet<byte>();
             for (byte i = 0; i < 255; i++)
             {
                 byte[] arr = test.Decode(test.Encode(new byte[1] { i }));
@@ -94,6 +99,106 @@ namespace NetHuffman
             }
         }
 
+        public void BuildDictionary(byte[] sample)
+        {
+            Dictionary<byte, uint> freq = new Dictionary<byte, uint>();
+            foreach (byte b in sample)
+            {
+                if (freq.ContainsKey(b))
+                {
+                    freq[b]++;
+                }
+                else
+                {
+                    freq.Add(b, 0); // Start at 0, this value is relative
+                }
+            }
+
+            List<KeyValuePair<byte, uint>> sortingList = new List<KeyValuePair<byte, uint>>(freq.Count);
+            foreach (KeyValuePair<byte, uint> kv in freq)
+            {
+                sortingList.Add(kv);
+            }
+
+            sortingList.Sort(DictionaryListComparer.Instance);
+
+            HashSet<uint> usedValues = new HashSet<uint>();
+            Dictionary<byte, Dictionary<uint, byte>> lookup = new Dictionary<byte, Dictionary<uint, byte>>();
+
+            byte currentbitlen;
+            if(Math.Max(Math.Ceiling(Math.Log(freq.Count) / Math.Log(2)), 1) > 8)
+            {
+                currentbitlen = 5;
+            }
+            else
+            {
+                for (currentbitlen = 1; currentbitlen < 8 && freq.Count > ((1 << (currentbitlen + 1)) / 2) + (((1 << (currentbitlen + 1)) - ((1 << (currentbitlen + 1)) / 2)) / 2); currentbitlen++) ;
+            }
+
+            uint value = 0;
+            sbyte shiftIndex = -1;
+            byte shiftedBits = 1;
+            bool shouldInvert = false;
+            int possibleUniques = 0x1 << currentbitlen;
+            unchecked
+            {
+                foreach (KeyValuePair<byte, uint> kv in sortingList)
+                {
+                    for (;;)
+                    {
+                        if(usedValues.Count == possibleUniques)
+                        {
+                            currentbitlen++;
+                            shiftIndex = 0;
+                            shiftedBits = 0;
+                            shouldInvert = false;
+                        }
+
+                        uint mask = (~((uint)0x0) >> (32 - currentbitlen));
+                        if (shouldInvert)
+                        {
+                            value = ~value & mask;
+                            shouldInvert = false;
+                            if(++shiftIndex == currentbitlen)
+                            {
+                                shiftedBits++;
+                                shiftIndex = 0;
+                            }
+                        }
+                        else
+                        {
+                            value = (0x0 | ~((uint)0x0) >> (32 - shiftedBits) << shiftIndex) & mask;
+                            shouldInvert = true;
+                        }
+
+                        if (!usedValues.Contains(value))
+                        {
+                            usedValues.Add(value);
+                            if(lookup.ContainsKey(currentbitlen))
+                            {
+                                lookup[currentbitlen].Add(value, kv.Key);
+                            }
+                            else
+                            {
+                                lookup.Add(currentbitlen, new Dictionary<uint, byte>()
+                                {
+                                    [value] = kv.Key
+                                });
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Init(lookup);
+        }
+
+        public void BuildDictionary(string sample)
+        {
+            BuildDictionary(Encoding.UTF8.GetBytes(sample));
+        }
+
         public class ReverseLookupEntry
         {
             public byte Value;
@@ -106,6 +211,16 @@ namespace NetHuffman
             public byte BitLength;
             public byte CurrentBits;
             public byte TotalBytes;
+        }
+
+        private class DictionaryListComparer : Comparer<KeyValuePair<byte, uint>>
+        {
+            public static DictionaryListComparer Instance = new DictionaryListComparer();
+
+            public override int Compare(KeyValuePair<byte, uint> x, KeyValuePair<byte, uint> y)
+            {
+                return y.Value.CompareTo(x.Value);
+            }
         }
     }
 }
---
+++ Testing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NetHuffman.Tree tree = new NetHuffman.Tree();
+
+            tree.BuildDictionary("this is a test");
+
+            NetHuffman.Coder coder = new NetHuffman.Coder(tree);
+
+            Console.WriteLine(Encoding.UTF8.GetString(coder.Decode(coder.Encode(Encoding.UTF8.GetBytes("this is a test")))));
+        }
+    }
+}
